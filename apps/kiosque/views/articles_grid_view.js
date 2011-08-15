74d6fe8e8f6f59572078b56dbb2601d23346106c
Cherif Yaya <chef.ya@gmail.com>
2011-08-15 15:12:04 +0200

    Added computation of column width and row height of articles grid. When in landscape have 5 by 2 grid of items. In Portrait use a 3 by 3 grid

--- apps/kiosque/views/articles_grid_view.js
+++ apps/kiosque/views/articles_grid_view.js
@@ -1,12 +1,17 @@
 // ==========================================================================
 // Project:   Kiosque.ArticlesGridView
-// Copyright: @2011 My Company, Inc.
+// Copyright: @2011 Strobe, Inc.
 // ==========================================================================
 /*globals Kiosque Iweb */
 require('system/array_page');
+
 /** @class
 
-  (Document Your View Here)
+  This class implements a layout of items (articles) in a grid-like fashion.
+  It supports paging the items. When in landscape have 5 by 2 grid of items
+  In Portrait use a 3 by 3 grid.
+  
+  The user can navigate through the pages by flicking left and right.
 
   @extends SC.View
 */
@@ -31,6 +36,12 @@ Kiosque.ArticlesGridView = Iweb.TabControlView.extend(
   /** @private */
   _pages: [],
   
+  /** @private spacing between items in the grid 10px*/
+  itemsSpacing: 10,
+  
+  /** @private */
+  pageExampleView: Kiosque.ArticlesPageView,
+  
   /** @private 
       Computes current orientation based on width. 
   */
@@ -43,14 +54,28 @@ Kiosque.ArticlesGridView = Iweb.TabControlView.extend(
   /** @private */
   columnWidth: function() {
     var frame = this.get('frame'),
+        itemsSpacing = this.get('itemsSpacing'),
         orientation = this.get('orientation') ;
+        
+    //When in landscape have 5 by 2 grid of items
+    //In Portrait use a 3 by 3 grid
+    var itemsPerRow = orientation === SC.HORIZONTAL_ORIENTATION ? 5 : 3 ;
+    var colWidth = (frame.width - (itemsSpacing * (itemsPerRow-1))) / itemsPerRow ;
     
-    
+    return colWidth ;
   }.property('orientation').cacheable(),
   
   /** @private */
   rowHeight: function() {
+    var frame = this.get('frame'),
+        itemsSpacing = this.get('itemsSpacing'),
+        orientation = this.get('orientation') ;
+    //When in landscape have 5 by 2 grid of items
+    //In Portrait use a 3 by 3 grid
+    var itemsPerCol = orientation === SC.HORIZONTAL_ORIENTATION ? 2 : 3 ;
+    var rowHeight = (frame.height - (itemsSpacing * (itemsPerCol-1))) / itemsPerCol ;
     
+    return rowHeight ;
   }.property('orientation').cacheable(),
   
   /** @private */
@@ -80,18 +105,22 @@ Kiosque.ArticlesGridView = Iweb.TabControlView.extend(
         totalItems = this.getPath('content.length'),
         nbPagesNeeded = Math.max(Math.ceil(totalItems*1.0/itemsPerPage), 1),
         nbPagesCreated = this._pages.get('length'),
-        content = this.get('content') ;
+        content = this.get('content'),
+        pageExampleView = this.get('pageExampleView') ;
     
     var page, i;
-       
+    
+    //Create and remove pages as needed
     if (nbPagesNeeded > nbPagesCreated) {
       //add missing pages
       for (i = nbPagesCreated; i < nbPagesNeeded; i++) {
         //create page
         page = this._pagesPool.pop();
         if (SC.none(page)) {
-          page = Kiosque.ArticlesPageView.create();
+          page = pageExampleView.create();
         }  
+        //add tab
+        this.addTab(page) ;
         
         //update page
         page.set('pageIndex', i) ;
@@ -103,12 +132,12 @@ Kiosque.ArticlesGridView = Iweb.TabControlView.extend(
         })) ;
         
         this._pages.push(page) ;
-        //add tab
-        this.addTab(page) ;
+        
       }
     }
     else if (nbPagesNeeded < nbPagesCreated) {
-      //removed 
+      //too many pages created. we don't need them all
+      //remove the extra
       for (i = nbPagesNeeded; i< nbPagesCreated; i++) {
         page = this._pages.pop();
         this.removeTab(page.get('pageIndex')) ;
@@ -154,5 +183,4 @@ Kiosque.ArticlesGridView = Iweb.TabControlView.extend(
       arrayPage.setIfChanged('itemsPerPage', itemsPerPage) ; 
     }
   }
-
 });
