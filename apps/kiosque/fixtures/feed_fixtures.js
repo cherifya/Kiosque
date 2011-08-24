0c307946e61c73d6623b0a640c2d53c61ba4ec34
Cherif Yaya <chef.ya@gmail.com>
2011-08-24 06:48:15 +0200

    Updates fixtures data with articles from The Unofficial Apple Weblog.

--- apps/kiosque/fixtures/feed_fixtures.js
+++ apps/kiosque/fixtures/feed_fixtures.js
@@ -7,11 +7,55 @@
 sc_require('models/feed_model');
 
 Kiosque.Feed.FIXTURES = [
-  {"articles": ["http://news.yahoo.com/rss/31-americans-7-afghans-killed-helicopter-crash-145151275", "http://news.yahoo.com/rss/p-downgrades-us-credit-rating-080422314", "http://news.yahoo.com/rss/rebels-launch-push-western-libya-aim-coast-090038113", "http://news.yahoo.com/rss/ap-sources-crash-kills-members-seal-team-6-155743903", "http://news.yahoo.com/rss/31-americans-7-afghans-killed-helicopter-crash-145151275", "http://news.yahoo.com/rss/group-says-hacked-us-law-enforcement-websites-153153045", "http://news.yahoo.com/rss/rebels-launch-push-western-libya-aim-coast-090038113", "http://news.yahoo.com/rss/p-downgrades-us-credit-rating-080422314", "http://news.yahoo.com/rss/key-figure-abu-ghraib-abuse-freed-prison-153601936", "http://news.yahoo.com/rss/perry-prayer-rally-puts-2012-prospect-spotlight-074113551", "http://news.yahoo.com/rss/job-worries-surge-debt-limit-issue-recedes-124917461", "http://news.yahoo.com/rss/spokesmen-islamist-militia-leaving-somali-capital-044748491", "http://news.yahoo.com/rss/irans-fm-hopes-release-2-americans-113539418", "http://news.yahoo.com/rss/p-reconsidering-u-downgrade-cnbc-001207261", "http://news.yahoo.com/rss/nato-airstrike-kills-8-afghan-civilians-police-051203129", "http://news.yahoo.com/rss/g7-major-powers-confer-markets-crisis-source-111015839", "http://news.yahoo.com/rss/federal-probes-mortgage-lenders-fizzle-report-145753355", "http://news.yahoo.com/rss/iran-foreign-minister-says-hopes-u-hikers-freed-113543520", "http://news.yahoo.com/rss/least-18-syria-protesters-killed-ramadan-friday-000647300", "http://news.yahoo.com/rss/somalias-al-shabaab-militia-leaving-mogadishu-073553109"], 
-  "updated": "2011-08-06T12:20:48-04:00", 
-  "description": "The latest news and headlines from Yahoo! News. Get breaking news stories and in-depth coverage with videos and photos.", 
-  "title": "Yahoo! News - Latest News & Headlines", 
-  "url": "http://news.yahoo.com/", 
-  "guid": "latest"}
+  {
+    "guid": "latest",
+    "updated": "-", 
+    "description": "TUAW - The Unofficial Apple Weblog", 
+    "title": "TUAW - The Unofficial Apple Weblog", 
+    "url": "http://www.tuaw.com",
+    "articles": [
+        "http://www.tuaw.com/2011/08/24/firemint-hosts-contest-win-a-chance-to-play-spy-mouse-first/", 
+        "http://www.tuaw.com/2011/08/23/bungie-details-ipad-release-of-crimson-steam-pirates-coming-ne/", 
+        "http://www.tuaw.com/2011/08/23/mcafee-android-malware-soars-while-iphone-untouched/", 
+        "http://www.tuaw.com/2011/08/23/dear-aunt-tuaw-help-me-ditch-the-screenshot-shadow/", 
+        "http://www.tuaw.com/2011/08/23/daily-mac-app-tremors/", 
+        "http://www.tuaw.com/2011/08/23/galaxy-s-ii-shows-android-stye-buttons-replacing-iphone-style-ho/", 
+        "http://www.tuaw.com/2011/08/23/os-x-lion-10-7-1-update-hits-mac-app-store-one-week-later/", 
+        "http://www.tuaw.com/2011/08/23/three-more-apple-stores-open-their-doors-this-saturday/", 
+        "http://www.tuaw.com/2011/08/23/wsj-sprint-will-sell-the-iphone-5-in-mid-october/", 
+        "http://www.tuaw.com/2011/08/23/big-us-retailers-adopting-apple-products-say-goodbye-to-the-cas/", 
+        "http://www.tuaw.com/2011/08/23/cnn-reportedly-buys-ipad-magazine-app-zite/", 
+        "http://www.tuaw.com/2011/08/23/chuckle-chuckle-samsung-claims-stanley-kubrick-invented-the-ipa/", 
+        "http://www.tuaw.com/2011/08/23/apple-streams-entire-red-hot-chili-peppers-album-in-itunes/", 
+        "http://www.tuaw.com/2011/08/23/daily-update-for-august-23-2011/", 
+        "http://www.tuaw.com/2011/08/23/iphone-poised-to-take-china-by-storm/", 
+        "http://www.tuaw.com/2011/08/23/united-and-continental-airlines-to-hand-out-11-000-ipads-to-its/", 
+        "http://www.tuaw.com/2011/08/23/mac-sales-climb-after-launch-of-os-x-lion-and-new-macbook-airs/", 
+        "http://www.tuaw.com/2011/08/23/apple-struggles-to-meet-13-inch-macbook-air-demand-resellers-ra/", 
+        "http://www.tuaw.com/2011/08/23/reuters-suggests-8gb-iphone-4-is-coming/", 
+        "http://www.tuaw.com/2011/08/23/nyc-restaurant-completely-reliant-on-ipad/", 
+        "http://www.tuaw.com/2011/08/23/counterpoint-the-ipad-is-the-third-device-id-hoped-for/", 
+        "http://www.tuaw.com/2011/08/23/os-x-lion-10-7-2-seed-2-now-available-for-developers/", 
+        "http://www.tuaw.com/2011/08/23/miramax-offers-video-rental-via-facebook-with-ipad-support/", 
+        "http://www.tuaw.com/2011/08/23/machine-gun-jetpack-now-jetpack-joyride-out-september-1/", 
+        "http://www.tuaw.com/2011/08/23/daily-iphone-app-musicpound/", 
+        "http://www.tuaw.com/2011/08/23/no-comment-the-iphone-megaphone/", 
+        "http://www.tuaw.com/2011/08/22/why-hibernate-or-safe-sleep-mode-is-no-longer-necessary-in-os/", 
+        "http://www.tuaw.com/2011/08/22/refurbished-ipads-now-just-299/", 
+        "http://www.tuaw.com/2011/08/22/apple-remote-desktop-admin-updated-to-3-5-1/", 
+        "http://www.tuaw.com/2011/08/22/urban-airship-delivers-5-billionth-push-notification/", 
+        "http://www.tuaw.com/2011/08/22/maxim-hd-for-ipad-to-halt-production-will-re-emerge-via-mag/", 
+        "http://www.tuaw.com/2011/08/22/lifeproof-iphone-4-case-submerge-your-iphone-and-smile/", 
+        "http://www.tuaw.com/2011/08/22/logmein-ignition-half-price-sale-and-a-tuaw-giveaway/", 
+        "http://www.tuaw.com/2011/08/22/ios-5-beta-6-gets-a-sn0wbreeze-for-windows-jailbreak/", 
+        "http://www.tuaw.com/2011/08/22/study-shows-iphone-owners-open-to-mobile-payments/", 
+        "http://www.tuaw.com/2011/08/22/daily-mac-app-war-pinball-hd/", 
+        "http://www.tuaw.com/2011/08/22/daily-update-for-august-22-2011/", 
+        "http://www.tuaw.com/2011/08/22/iphone-4-3gs-outsell-competition-in-q2-says-npd-group/", 
+        "http://www.tuaw.com/2011/08/22/evernote-ios-app-update-offers-rich-text-support-shared-noteboo/", 
+        "http://www.tuaw.com/2011/08/22/daily-ipad-app-iclassics/"
+      ]
+  
+  }
 
 ];
