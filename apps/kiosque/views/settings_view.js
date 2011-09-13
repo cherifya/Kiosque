ad1d3197e31016326a33c48fe716505cadbbd105
Cherif Yaya <chef.ya@gmail.com>
2011-09-14 01:08:27 +0200

    Add feeds list to settings panel.

--- apps/kiosque/views/settings_view.js
+++ apps/kiosque/views/settings_view.js
@@ -13,6 +13,24 @@
 Kiosque.SettingsView = SC.View.extend(
 /** @scope Kiosque.SettingsView.prototype */ {
 
+  classNames: ''.w(),
+  childViews: 'feedsLabel scroll'.w(),
+  
+  feedsLabel: SC.LabelView.design({
+    layout: {top:10,left:10,height:24,width:200},
+    value: 'Feeds'
+  }),
+  
+  scroll: SC.ScrollView.design({
+    layout: {top:50,right:10,left:10,height:150},
+    
+    contentView: SC.ListView.design({
+      layout: {top:0,right:0,bottom:0,left:0},
+      contentBinding: 'Kiosque.sourcesController.arrangedObjects',
+      //selectionBinding: 'Kiosque.sourcesController.selection'
+      contentValueKey: 'name'
+    })
+  })
   
 
 });
